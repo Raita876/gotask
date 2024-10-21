3d0cb885a11e70a09b3bb5cb30437914b5a81797
Raita Watanabe <watanaberit20171129@gmail.com>
2024-10-22 02:56:07 +0900

    [fix] infra

--- internal/infra/db/mysql/task.go
+++ internal/infra/db/mysql/task.go
@@ -4,9 +4,10 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/raita876/gotask/internal/domain"
 )
 
-type Task struct {
+type DBTask struct {
 	ID        uuid.UUID `gorm:"column:id;type:char(36);primaryKey"`
 	Name      string    `gorm:"column:name;not null"`
 	Status    uint16    `gorm:"column:status;not null"`
@@ -15,6 +16,28 @@ type Task struct {
 	UpdatedAt time.Time `gorm:"column:updated_at;not null"`
 }
 
-func (Task) TableName() string {
+func (DBTask) TableName() string {
 	return "tasks"
+}
+
+func toDBTask(task *domain.Task) *DBTask {
+	return &DBTask{
+		ID:        task.ID,
+		Name:      task.Name,
+		Status:    task.Status,
+		UserID:    task.UserID,
+		CreatedAt: task.CreatedAt,
+		UpdatedAt: task.UpdatedAt,
+	}
+}
+
+func fromDBTask(dbTask *DBTask) *domain.Task {
+	return &domain.Task{
+		ID:        dbTask.ID,
+		Name:      dbTask.Name,
+		Status:    dbTask.Status,
+		UserID:    dbTask.UserID,
+		CreatedAt: dbTask.CreatedAt,
+		UpdatedAt: dbTask.UpdatedAt,
+	}
 }
--- internal/infra/db/mysql/user.go
+++ internal/infra/db/mysql/user.go
@@ -4,9 +4,10 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/raita876/gotask/internal/domain"
 )
 
-type User struct {
+type DBUser struct {
 	ID        uuid.UUID `gorm:"column:id;type:char(36);primaryKey"`
 	Name      string    `gorm:"column:name;not null"`
 	Email     string    `gorm:"column:email;not null"`
@@ -15,6 +16,28 @@ type User struct {
 	UpdatedAt time.Time `gorm:"column:updated_at;not null"`
 }
 
-func (User) TableName() string {
+func (DBUser) TableName() string {
 	return "users"
+}
+
+func toDBUser(user *domain.User) *DBUser {
+	return &DBUser{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		Password:  user.Password,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
+
+func fromDBUser(dbUser *DBUser) *domain.User {
+	return &domain.User{
+		ID:        dbUser.ID,
+		Name:      dbUser.Name,
+		Email:     dbUser.Email,
+		Password:  dbUser.Password,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+	}
 }
